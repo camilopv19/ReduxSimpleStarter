e1f2aabd39ef1b60336a278f513f0cf1f78d1d09
Camilo P <kmilopv@gmail.com>
2018-10-31 20:28:29 -0500

    Starting
    Lecture 10

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+//Create a new component which produces HTML code
+const App = function(){ //CLASS: "App"
+    return <div>Hi!</div>;
+}
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Takes the component's generated HTML and
+//puts it on the page (in the DOM)
+ReactDOM.render(<App />); //Self enclosing Tag for an INSTANCES of the CLASS/Component "App"
